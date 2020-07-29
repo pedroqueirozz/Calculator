4d19d2825541648457203bdb78bd7785efa2276f
pedroqueirozz <pedroroquei@yahoo.com.br>
2020-07-29 12:56:41 -0300

    Almost done
    JS working correctly and some css effects done.

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,5 @@
 let visor = document.getElementById('label')
+let result = ''
 
 let clear = document.querySelector('.clear')
 let percentage = document.querySelector('.percentage')
@@ -36,72 +37,103 @@ plus.addEventListener('click', plusFn)
 one.addEventListener('click', oneFn)
 two.addEventListener('click', twoFn)
 three.addEventListener('click', threeFn)
-// change.addEventListener('click', changeFn)
+change.addEventListener('click', changeFn)
 zero.addEventListener('click', zeroFn)
 comma.addEventListener('click', commaFn)
-// equal.addEventListener('click', equalFn)
+equal.addEventListener('click', equalFn)
 
 
-function clearFn() {
-    visor.value = ''
-}
 function percentageFn() {
     visor.value = visor.value + '%'
 }
+
 function divideFn() {
     visor.value = visor.value + '/'
+    result = result + '/'
 }
+
 function multiplyFn() {
     visor.value = visor.value + 'x'
+    result = result + '*'
 }
+
 function sevenFn() {
     visor.value = visor.value + '7'
+    result = result + '7'
 }
+
 function eightFn() {
     visor.value = visor.value + '8'
+    result = result + '8'
 }
+
 function nineFn() {
     visor.value = visor.value + '9'
+    result = result + '9'
 }
+
 function minusFn() {
     visor.value = visor.value + '-'
+    result = result + '-'
 }
+
 function fourFn() {
     visor.value = visor.value + '4'
+    result = result + '4'
 }
+
 function fiveFn() {
     visor.value = visor.value + '5'
+    result = result + '5'
 }
+
 function sixFn() {
     visor.value = visor.value + '6'
+    result = result + '6'
 }
+
 function plusFn() {
     visor.value = visor.value + '+'
+    result = result + '+'
 }
+
 function oneFn() {
     visor.value = visor.value + '1'
+    result = result + '1'
 }
+
 function twoFn() {
     visor.value = visor.value + '2'
+    result = result + '2'
 }
+
 function threeFn() {
     visor.value = visor.value + '3'
+    result = result + '3'
 }
-// function changeFn() {
-//     visor.value = visor.value + ''
-// }
+
 function zeroFn() {
     visor.value = visor.value + '0'
+    result = result + '0'
 }
+
 function commaFn() {
-    visor.value = visor.value + '.'
+    visor.value = visor.value + ','
+    result = result + '.'
 }
 
-// function equalFn() {
-//     visor.value = visor.value + '.'
-// }
+function clearFn() {
+    visor.value = ''
+    result = ''
+}
 
+function changeFn() {
+    visor.value = eval(visor.value) * (-1)  // (Number(visor.value) * (-1)).toString()
+    console.log(eval(visor.value))
+}
 
-
-
+function equalFn() {
+    console.log(eval(result))
+    visor.value = eval(result)
+}
 
